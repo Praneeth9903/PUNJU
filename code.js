c5b6145bb2de767dc0a3f03fd7cd1ccbdcb0ec5e
Praneeth9903 <78412027+Praneeth9903@users.noreply.github.com>
2021-04-22 08:27:55 +0530

    Add files via upload

---
+++ code.js
@@ -0,0 +1,9 @@
+onEvent("started", "click", function() {
+  setScreen("FRIENDS1");
+});
+onEvent("PUNJUMP3", "click", function(){
+playSound("assets/Unq-Gamer-!-Funny-!-Music-!-Song.mp3");
+});
+onEvent("OKNEXT", "click", function(){
+setScreen("thepunju");
+});
